8cb0038d35910b23392e26c24bd2bf784dac226d
Kawchar Husain <52884694+kawchar85@users.noreply.github.com>
2020-09-10 00:56:30 +0600

    Create coin_change2.cpp

---
+++ Dynamic_Programming/coin_change2.cpp
@@ -0,0 +1,29 @@
+///minimum coin
+#include<bits/stdc++.h>
+#define MAX 10000
+#define INF 1000000
+using namespace std;
+#define N 5
+int coin[N]={1,5,10,25,50},DP[N][MAX];
+
+///O(i*tk)
+int solve(int i, int taka)
+{
+    if(taka<0 || i>=N)
+        return INF;
+    if(taka==0)
+        return 0;
+    if(DP[i][taka]!=-1)
+        return DP[i][taka];
+    return DP[i][taka]=min(1+solve(i,taka-coin[i]),solve(i+1,taka));
+}
+int main()
+{
+    memset(DP,-1,sizeof DP);
+    int tk;
+    while(~scanf("%d",&tk))
+    {
+        printf("%d\n",solve(0,tk));
+    }
+    return 0;
+}
