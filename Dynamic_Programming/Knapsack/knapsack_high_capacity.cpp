16eb85689bd97a053f2eee16fc2dfec54ce3ddbc
Kawchar Husain <52884694+kawchar85@users.noreply.github.com>
2020-09-10 02:24:56 +0600

    Update knapsack_high_capacity.cpp

--- Dynamic_Programming/Knapsack/knapsack_high_capacity.cpp
+++ Dynamic_Programming/Knapsack/knapsack_high_capacity.cpp
@@ -6,13 +6,14 @@
 */
 #include <bits/stdc++.h>
 using namespace std;
-#define maxN 100
-#define maxV 1000
+#define maxN 1000
+#define maxV 1003
 #define INF 1e17
 #define ll long long 
 #define endl "\n"
 
 ll dp[maxN*maxV+5];
+vector<ll> wt,profit;
 
 int main()
 {   
@@ -20,11 +21,22 @@ int main()
 
     int i,j,n,ans=0;
     for(i=1; i<=maxN*maxV; i++) dp[i] = INF;
-    ll cap,sum=0,v,w;
+    ll cap,sum=0,v,w,x;
     cin>>n>>cap;
     for(i=0; i<n; i++)
     {
-        cin>>w>>v;
+        cin>>x;
+        wt.push_back(x);
+    }
+    for(i=0; i<n; i++)
+    {
+        cin>>x;
+        profit.push_back(x);
+    }
+    for(i=0; i<n; i++)
+    {
+        w=wt[i];
+        v=profit[i];
         sum+=v;
         for(j=sum; j>=v; j--)
         {
