c4de82a5f6617fb79b05a2631d15360e46b923c4
Kawchar Husain <52884694+kawchar85@users.noreply.github.com>
2020-09-11 22:18:37 +0600

    Update coin_change2.cpp

--- Dynamic_Programming/Coin_Change/coin_change2.cpp
+++ Dynamic_Programming/Coin_Change/coin_change2.cpp
@@ -9,10 +9,10 @@ int coin[N]={1,5,10,25,50},DP[N][MAX];
 ///O(i*tk)
 int solve(int i, int taka)
 {
-    if(taka<0 || i>=N)
-        return INF;
     if(taka==0)
         return 0;
+    if(taka<0 || i>=N)
+        return INF;
     if(DP[i][taka]!=-1)
         return DP[i][taka];
     return DP[i][taka]=min(1+solve(i,taka-coin[i]),solve(i+1,taka));
