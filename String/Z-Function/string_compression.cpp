2056abe78dc2d59560ecfbe7353a8b5ef3bc565d
Kawchar Husain <52884694+kawchar85@users.noreply.github.com>
2020-12-23 17:13:31 +0600

    Create string_compression.cpp

---
+++ String/Z-Function/string_compression.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+///O(s.size())
+vector<int> z_function(string s)
+{
+    int n = (int) s.length();
+    vector<int> z(n+1);
+    for(int i = 1, l = 0, r = 0; i < n; ++i) {
+        if (i <= r)
+            z[i] = min (r - i + 1, z[i - l]);
+        while(i + z[i] < n && s[z[i]] == s[i + z[i]])
+            ++z[i];
+        if (i + z[i] - 1 > r)
+            l = i, r = i + z[i] - 1;
+    }
+    return z;
+}
+
+int main()
+{
+    string s;
+    int i,n;
+    cin>>s;
+    n=s.size();
+    vector<int> z = z_function(s);
+
+    for(i=1; i<=n; i++)
+    {
+        if(n%i==0 && i+z[i]==n)
+        {
+            cout<<i<<endl;
+            break;
+        }
+    }
+
+
+    return 0;
+}
+
