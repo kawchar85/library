1a15075b493e3fd454ed2c105a26136c4e42818a
Kawchar Husain <52884694+kawchar85@users.noreply.github.com>
2021-01-18 20:54:50 +0600

    Create aho.cpp

---
+++ String/Aho-Corasick/aho.cpp
@@ -0,0 +1,92 @@
+#include<bits/stdc++.h>
+#define MAX 200005
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+
+#define t_sz 26
+int counter=1;
+
+struct node{
+    int ending;
+    int next[t_sz];
+    int link,par;
+    node()
+    {
+        ending=0;
+        memset(next,-1, sizeof next);
+        par=-1;
+        link=-1;
+    }
+}aho[MAX];
+
+int ID(char ch){
+    if('a'<=ch && ch<='z')
+        return ch-'a';
+    return ch-'A';
+}
+
+void ADD(string &s)
+{
+    int now,n=0;
+    for(auto ch:s)
+    {
+        now=ID(ch);
+        if(aho[n].next[now]==-1)
+            aho[n].next[now]=counter++;
+        n=aho[n].next[now];
+    }
+
+    aho[n].ending=1;
+}
+
+int transition(int u, int i) //from,by
+{
+    if(u==-1) return 0; //root
+    if(~aho[u].next[i]) return aho[u].next[i];
+    return aho[u].next[i]=transition(aho[u].link, i);
+}
+void push_links()
+{
+    queue<int> q;
+    q.push(0);
+    while(!q.empty())
+    {
+        int u=q.front();
+        q.pop();
+        for(int i=0; i<t_sz; i++)
+        {
+            int v=aho[u].next[i];
+            if(v==-1) continue;
+
+            aho[v].par=u;
+            aho[v].link=transition(aho[u].link, i);
+            aho[v].ending+=aho[aho[v].link].ending;
+            q.push(v);
+        }
+    }
+}
+int CNT(vector<int> &v)
+{
+    int u=0,sum=0;
+    for(auto x : v)
+    {
+        u=transition(u, x);
+        sum+=aho[u].ending;
+    }
+    return sum;
+}
+
+void delete_aho()
+{
+    counter=1;
+    memset(aho, -1, sizeof aho);
+}
+
+int main()
+{
+    FAST
+
+
+    
+    return 0;
+}
