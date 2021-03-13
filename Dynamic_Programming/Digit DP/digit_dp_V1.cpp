12df48f4387d0a6b774a163877848f5be25febb8
Kawchar Husain <52884694+kawchar85@users.noreply.github.com>
2021-03-13 21:55:25 +0600

    Create digit_dp_V1.cpp

---
+++ Dynamic_Programming/Digit DP/digit_dp_V1.cpp
@@ -0,0 +1,64 @@
+/**
+    Find the number of integers between [1, n]
+    satisfying the following condition:
+    The sum of the digits is a multiple of D
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+#define ll long long 
+
+vector<int> num;
+ll DP[20][100][2][2],d;
+
+ll call(int pos, int sum, int small, int startHere)
+{
+    if(pos == (int)num.size())
+        return sum%d==0;
+    if(~DP[pos][sum][small][startHere]) 
+        return DP[pos][sum][small][startHere];
+
+    ll res = 0;
+    int LMT = small? 9 : num[pos];
+
+    if(startHere)
+    {
+        res+=call(pos+1, 0, 1, 1);
+    }
+    for(int dgt=startHere; dgt<=LMT; dgt++)
+    {
+        res = (res + call(pos+1, (sum+dgt)%d, small|(dgt<num[pos]), 0))%MOD;
+    }
+ 
+    return DP[pos][sum][small][startHere]=res;
+}
+ll solve(ll b)
+{
+    num.clear();
+    while(b>0)
+    {
+        num.push_back(b%10);
+        b/=10;
+    }
+    reverse(num.begin(), num.end());
+
+    memset(DP, -1, sizeof(DP));
+    return (MOD+call(0, 0, 1)-1)%MOD; //-1 for 0
+}
+/*
+ll solve(string& s)
+{
+    num.clear();
+    for(int i=0; i<s.size(); i++)
+        num.push_back(s[i]-'0');
+    memset(DP, -1, sizeof(DP));
+    return (call(0, 0, 1)-1+MOD)%MOD;
+}
+*/
+int main ()
+{
+    ll n;
+    cin>>n>>d;
+    cout<<solve(n)<<endl;
+    return 0;
+}
