447675c4e85d1e47a122bf19b62f7bfc59f6e70f
Kawchar Husain <52884694+kawchar85@users.noreply.github.com>
2021-03-25 01:41:14 +0600

    Create prac_2_cnt_total.cpp

---
+++ String/Palindromic Tree/prac_2_cnt_total.cpp
@@ -0,0 +1,105 @@
+//https://vjudge.net/problem/SPOJ-NUMOFPAL
+
+#include<iostream>
+#include<string>
+#include<string.h>
+using namespace std;
+
+const int N = 1005;
+const int t_sz= 26;
+
+struct Palindromic_Tree{
+    
+    int tree[N][t_sz], idx;
+    int len[N], link[N], t,n;
+    int endHere[N],occ[N],total=0;
+    string s="#";
+
+    Palindromic_Tree(){
+        memset(occ, 0, sizeof(occ));
+        len[1] = -1, link[1] = 1;
+        len[2] = 0, link[2] = 1;
+        endHere[1]=endHere[2]=0;
+        idx = t = 2;
+    }
+
+    void add(int p)
+    {
+        while(s[p-len[t]-1] != s[p]) t = link[t];
+        int x = link[t], c = s[p] - 'a';
+        while(s[p-len[x]-1] != s[p]) x = link[x];
+        
+        if(!tree[t][c])
+        {
+            tree[t][c] = ++idx;
+            len[idx] = len[t] + 2;
+            link[idx] = len[idx] == 1 ? 2 : tree[x][c];
+            endHere[idx]=1+endHere[link[idx]];
+        }
+        
+        t = tree[t][c];
+        occ[t]++;
+    }
+
+    void init(string &ss)
+    {
+        s+=ss;
+        n=(int)s.size();
+        
+        for(int i=1; i<n; i++){
+            add(i);
+            total+=endHere[t];
+            //palindrom end in pos i=endHere[t]
+            //cout<<i<<"-->"<<endHere[t]<<endl;
+        }
+        for(int i = idx; i > 2; i--) 
+            occ[link[i]] += occ[i];
+
+        //print();
+    }
+    void print(){
+        for(int i=3; i<=idx; i++)
+            cout<<"len="<<len[i]<<",cnt="<<occ[i]<<endl;
+        
+    }
+    void clear(){
+        for(int i=0; i<=idx; i++)
+        {
+            occ[i]=endHere[i]=len[i]=link[i]=0;
+            for(int j=0; j<t_sz; j++)
+                tree[i][j]=0;
+        }
+
+        len[1] = -1, link[1] = 1;
+        len[2] = 0, link[2] = 1;
+        endHere[1]=endHere[2]=0;
+        idx = t = 2;
+        total=0;
+        s="#"; 
+    }
+    int cntDistinct(){
+        return idx-2;
+    }
+    int cntTotal(){
+        return total;
+    }
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    int t=1;
+    string s;
+    Palindromic_Tree p;
+    //cin>>t;
+    for(int cs=1; cs<=t; cs++)
+    {
+        cin>>s;
+        p.init(s);
+        cout<<p.cntTotal()<<endl;
+        //cout<<"Case #"<<cs<<": "<<p.cntDistinct()<<endl;
+        p.clear();
+    }
+    return 0;
+}
