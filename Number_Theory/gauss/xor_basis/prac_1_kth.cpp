ea61b9d2c6c3e891b6a49b57e304a15751f5d510
Kawchar Husain <52884694+kawchar85@users.noreply.github.com>
2021-04-19 03:13:00 +0600

    Create prac_1_kth.cpp

---
+++ Number_Theory/gauss/xor_basis/prac_1_kth.cpp
@@ -0,0 +1,65 @@
+//https://codeforces.com/group/qcIqFPYhVr/contest/203881/problem/S
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define endl "\n"
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+struct XorBasis{
+	vector<ll> basis;
+	ll N=0;
+	void add(ll x){
+		N++;
+		for(auto &i : basis) x=min(x,x^i);
+		if(!x) return;
+		for(auto &i : basis) if((i^x)<i) i^=x;
+	    basis.push_back(x);
+	    sort(basis.begin(), basis.end());
+	}
+	ll size(){
+		return (ll)basis.size();
+	}
+	void clear(){
+		N=0;
+		basis.clear();
+	}
+	bool possible(ll x){
+	    for(auto &i: basis) x=min(x, x^i);
+	    return !x;
+	}
+	ll maxxor(ll x=0){
+	    for(auto &i: basis) x=max(x, x^i);
+		return x;
+	}
+	ll minxor(ll x=0){
+	    for(auto &i: basis) x=min(x, x^i);
+		return x;
+	}
+	ll cntxor(ll x){
+		if(!possible(x)) return 0LL;
+		return (1LL<<(N-size()));
+	}
+	ll kth(ll k){
+	    ll sz=size();
+	    if(k > (1LL<<sz)) return -1;
+	    k--; ll ans=0;
+	    for(ll i=0; i<sz; i++) if(k>>i & 1) ans^=basis[i];
+	    return ans;
+	}
+}xb;
+
+int main(){
+	FAST;
+
+	ll n,type,k;
+	cin>>n;
+	while(n--)
+	{
+		cin>>type>>k;
+		if(type==1)
+			xb.add(k);
+		else
+			cout<<xb.kth(k)<<endl;
+	}
+}
