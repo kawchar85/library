a0b76a4a886f3be8b5bda4ac91ba6909200a8a96
Kawchar Husain <52884694+kawchar85@users.noreply.github.com>
2021-04-19 14:18:17 +0600

    Update xor_basis.cpp

--- Number_Theory/gauss/xor_basis/xor_basis.cpp
+++ Number_Theory/gauss/xor_basis/xor_basis.cpp
@@ -3,57 +3,62 @@ using namespace std;
 #define ll long long 
 
 struct XorBasis{
-	vector<ll> basis;
-	ll N=0;
-	void add(ll x){
-		N++;
-		for(auto &i : basis) x=min(x,x^i);
-		if(!x) return;
-		for(auto &i : basis) if((i^x)<i) i^=x;
-	    basis.push_back(x);
-	    sort(basis.begin(), basis.end());
-	}
-	ll size(){
-		return (ll)basis.size();
-	}
-	void clear(){
-		N=0;
-		basis.clear();
-	}
-	bool possible(ll x){
-	    for(auto &i: basis) x=min(x, x^i);
-	    return !x;
-	}
-	ll maxxor(ll x=0){
-	    for(auto &i: basis) x=max(x, x^i);
-		return x;
-	}
-	ll minxor(ll x=0){
-	    for(auto &i: basis) x=min(x, x^i);
-		return x;
-	}
-	ll cntxor(ll x){
-		if(!possible(x)) return 0LL;
-		return (1LL<<(N-size()));
-	}
-	ll kth(ll k){
-	    ll sz=size();
-	    if(k > (1LL<<sz)) return -1;
-	    k--; ll ans=0;
-	    for(ll i=0; i<sz; i++) if(k>>i & 1) ans^=basis[i];
-	    return ans;
-	}
+    vector<ll> basis;
+    ll N=0,tmp=0;
+    void add(ll x){
+        N++;tmp|=x;
+        for(auto &i : basis) x=min(x,x^i);
+        if(!x) return;
+        for(auto &i : basis) if((i^x)<i) i^=x;
+        basis.push_back(x);
+        sort(basis.begin(), basis.end());
+    }
+    ll size(){
+        return (ll)basis.size();
+    }
+    void clear(){
+        N=0;tmp=0;
+        basis.clear();
+    }
+    bool possible(ll x){
+        for(auto &i: basis) x=min(x, x^i);
+        return !x;
+    }
+    ll maxxor(ll x=0){
+        for(auto &i: basis) x=max(x, x^i);
+        return x;
+    }
+    ll minxor(ll x=0){
+        for(auto &i: basis) x=min(x, x^i);
+        return x;
+    }
+    ll cntxor(ll x){
+        if(!possible(x)) return 0LL;
+        return (1LL<<(N-size()));
+    }
+    ll sumOfAll(){
+        ll ans=tmp*(1LL<<(N-1));
+        return ans;
+    }
+    ll kth(ll k){
+        ll sz=size();
+        if(k > (1LL<<sz)) return -1;
+        k--; ll ans=0;
+        for(ll i=0; i<sz; i++) if(k>>i & 1) ans^=basis[i];
+        return ans;
+    }
 }xb;
 
 int main(){
-	ll n,type,k;
-	cin>>n;
-	while(n--)
-	{
-		cin>>type>>k;
-		if(type==1)
-			xb.add(k);
-		else
-			cout<<xb.kth(k)<<endl;
-	}
+    ll n,type,k;
+    cin>>n;
+    while(n--)
+    {
+        cin>>type>>k;
+        if(type==1)
+            xb.add(k);
+        else
+            cout<<xb.kth(k)<<endl;
+    }
+    cout<<xb.sumOfAll()<<endl;
 }
