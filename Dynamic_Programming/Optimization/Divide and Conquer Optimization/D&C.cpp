93cb7e1a44f9e1b36554e85aaac63f7054a36557
Kawchar Husain <52884694+kawchar85@users.noreply.github.com>
2022-07-09 21:28:42 +0600

    Create D&C.cpp

---
+++ Dynamic_Programming/Optimization/Divide and Conquer Optimization/D&C.cpp
@@ -0,0 +1,26 @@
+//https://robert1003.github.io/2020/02/25/dp-opt-divide-and-conquer.html
+ll dp[N];
+ll cost(int l, int r){
+  return 1;
+}
+
+void solve(int l, int r, int ql, int qr) {
+  if(l > r) return;
+  int mid = l + (r - l) / 2;
+  
+  ll pos =0, val = INT_MAX;
+  for(int k = ql; k <= min(mid, qr); k++) {
+    ll ret = cost(k, mid);
+    if(ret < val) val = ret, pos = k;
+  }
+
+  dp[mid] = val;
+
+  // for H[j]<=H[j+1] i.e. cost(1,j)<=cost(1,j+1)
+  solve(l, mid - 1, ql, pos);
+  solve(mid + 1, r, pos, qr);
+  // for H[j]>=H[j+1] i.e. cost(1,j)>=cost(1,j+1)
+  //solve(l, mid - 1, pos, qr);
+  //solve(mid + 1, r, ql, pos);
+
+}
