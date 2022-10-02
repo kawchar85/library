976435867cc4f4c160383988840f1ea4b77ad28c
Kawchar Husain <52884694+kawchar85@users.noreply.github.com>
2022-10-03 01:47:11 +0600

    clean up

--- Data Structure and Graphs/Treap/treap.cpp
+++ Data Structure and Graphs/Treap/treap.cpp
@@ -6,22 +6,20 @@ using namespace std;
 #define MAX 200005
 typedef long long ll;
 
-ll F(ll x, ll y){
-    return x + y;
-}
 struct info{
-    int l, r, prt, sz, val, ans;
+    int l, r, prt, sz, val, ans, rev;
 };
 
 //1-Based index
 int N, treap;
 info tree[MAX];
 
-void clear(){ N = 0; treap = -1; srand(time(0));}
+void init(){ N = 0; treap = -1; srand(time(0));}
 
 int get_node(int val){
     tree[N].val = val;
     tree[N].ans = val;
+    tree[N].rev = 0;
     tree[N].sz = 1;
     tree[N].l = -1;
     tree[N].r = -1;
@@ -30,42 +28,83 @@ int get_node(int val){
     return N++;
 }
 
-void calibrate(int i){
-    if(i == -1) return;
-    tree[i].sz = 1;
-    int l = tree[i].l, r = tree[i].r;
-    tree[i].ans = tree[i].val;
-    if(~l){
-        tree[i].sz += tree[l].sz;
-        tree[i].ans = F(tree[l].ans,  tree[i].ans);
+void propagate(int t){
+    //push down: T-->L,R
+    if(t == -1 || !tree[t].rev) return;
+    int l = tree[t].l, r = tree[t].r;
+    if(~l) tree[l].rev ^= 1; 
+    if(~r) tree[r].rev ^= 1;
+    swap(tree[t].l, tree[t].r);
+    tree[t].rev = 0;
+}
+
+void calibrate(int t){
+    //push up: L,R-->Par
+    propagate(t); //need?
+    if(t == -1) return;
+    tree[t].sz = 1;
+    int l = tree[t].l, r = tree[t].r;
+    //update values
+    tree[t].ans = tree[t].val;
+    if(~l){ //Combine Lft,Par
+        tree[t].sz += tree[l].sz;
+        tree[t].ans = tree[l].ans + tree[t].ans;
     }
-    if(~r){
-        tree[i].sz += tree[r].sz;
-        tree[i].ans = F(tree[i].ans, tree[r].ans);
+    if(~r){ //Combine Par,Rgt
+        tree[t].sz += tree[r].sz;
+        tree[t].ans = tree[t].ans + tree[r].ans;
     }
 }
 
 pair<int,int> split(int t, int key){
+    propagate(t);
     if(t == -1) return {-1, -1};
-    if(key == 0) return {-1, t};
+    if(key <= 0) return {-1, t};
     pair<int,int> ret;
     int l = tree[t].l, r = tree[t].r;
     if(l == -1 || tree[l].sz < key){
         ret = split(r, key - (~l ? tree[l].sz : 0) - 1);
         tree[t].r = ret.first;
         ret.first = t;
-        calibrate(t);
     }else{
         ret = split(l, key);
         tree[t].l = ret.second;
         ret.second = t;
-        calibrate(t);
     }
+    calibrate(t);
     return ret;
 }
+//only for sorted list
+pair<int,int> split_by_value(int t, int val){
+    propagate(t);
+    if(t == -1) return {-1, -1};
+    pair<int,int> ret;
+    int l = tree[t].l, r = tree[t].r;
+    if(tree[t].val <= val){
+        ret = split_by_value(r, val);
+        tree[t].r = ret.first;
+        ret.first = t;
+    }else{
+        ret = split_by_value(l, val);
+        tree[t].l = ret.second;
+        ret.second = t;
+    }
+    calibrate(t);
+    return ret;
+}
+//v[0] = [1, l-1], v[1] = [l, r], v[2] = [r+1, N]
+vector<int> parts(int l, int r){
+    vector<int> v(3);
+    auto cur = split(treap, l - 1);
+    v[0] = cur.first;
+    cur = split(cur.second, r - l + 1);
+    v[1] = cur.first;
+    v[2] = cur.second;
+    return v;
+}
 int merge(int a,int b){
-    if(a == -1) return b;
-    if(b == -1) return a;
+    propagate(a); propagate(b);
+    if(min(a, b) == -1) return max(a, b);
     if(tree[a].prt > tree[b].prt){
         tree[a].r = merge(tree[a].r, b);
         calibrate(a);
@@ -76,70 +115,88 @@ int merge(int a,int b){
         return b;
     }
 }
-
+int merge(int a, int b, int c){
+    return merge(merge(a, b), c);
+}
 int query(int l, int r){
-    int a, b, c;
-    auto cur = split(treap, l - 1);
-    a = cur.first;
-    cur = split(cur.second, r - l + 1);
-    b = cur.first;
-    c = cur.second;
-    int ret = tree[b].ans;
-    a = merge(a, b);
-    a = merge(a, c);
-    treap = a;
-    return ret;
+    auto p = parts(l, r);
+    int ret = tree[ p[1] ].ans;
+    treap = merge(p[0], p[1], p[2]);
+    return ret ;
 }
-
+void move_to_beg(int l, int r){
+    auto p = parts(l, r);
+    treap = merge(p[1], p[0], p[2]);
+}
 void erase(int l, int r){
-    int a, b, c;
-    auto cur = split(treap, l - 1);
-    a = cur.first;
-    cur = split(cur.second, r - l + 1);
-    b = cur.first;
-    c = cur.second;
-    a = merge(a, c);
-    treap = a;
+    auto p = parts(l, r);
+    treap = merge(p[0], p[2]);
 }
-
 void update(int i, int val){
-    int a, b, c;
-    auto cur = split(treap, i - 1);
-    a = cur.first;
-    cur = split(cur.second, 1);
-    b = cur.first;
-    tree[b].val = val;
-    calibrate(b);
-    c = cur.second;
-    a = merge(a, b);
-    a = merge(a, c);
-    treap = a;
+    auto p = parts(i, i);
+    tree[ p[1] ].val = val;
+    calibrate(p[1]);
+    treap = merge(p[0], p[1], p[2]);
+}
+void reverse(int l, int r){
+    auto p = parts(l, r);
+    tree[ p[1] ].rev ^= 1;
+    treap = merge(p[0], p[1], p[2]);
+}
+void cyclicShift(int l, int r, int cnt = 1, int left_shift = 0){
+    auto p = parts(l, r);
+    if(left_shift) cnt = r - l + 1 - cnt;
+    auto cur = split(p[1], cnt);
+    p[1] = merge(cur.second, cur.first);
+    treap = merge(p[0], p[1], p[2]);
 }
 void insert(int i, int val){
-    auto ret = split(treap, i - 1);
     int x = get_node(val);
-    int t = merge(ret.first, x);
-    t = merge(t, ret.second);
-    treap = t;
+    auto p = parts(i, i);
+    p[1] = merge(x, p[1]);
+    treap = merge(p[0], p[1], p[2]);
 }
-
-void print(int i = treap, int last = 1){
-    if(i == -1) return;
-    print(tree[i].l, 0);
-    cout << tree[i].val << ' ';
-    print(tree[i].r, 0);
+void push_back(int val){
+    treap = merge(treap, get_node(val));
+}
+void print(int t = treap, int last = 1){
+    propagate(t);
+    if(t == -1) return;
+    print(tree[t].l, 0);
+    cout << tree[t].val << ' ';
+    print(tree[t].r, 0);
     if(last) cout << "\n";
+}
+//for sorted list
+int lower_bound(int val){
+    //[<val][val][>val]
+    auto p = split_by_value(treap, val);
+    auto p2 = split_by_value(p.first, val-1);
+    int pos = 1;
+    if(~p2.first) pos += tree[p2.first].sz;
+    treap = merge(p2.first, p2.second, p.second);
+    return pos;
+}
+int find_pos(int val){
+    //[<val][val][>val]
+    auto p = split_by_value(treap, val);
+    auto p2 = split_by_value(p.first, val-1);
+    int pos = 1;
+    if(p2.second == -1) pos = -1;
+    else if(~p2.first) pos += tree[p2.first].sz;
+    treap = merge(p2.first, p2.second, p.second);
+    return pos;
 }
 
 void solve(){
 
-    clear();
+    init();
     ll n, x;
 
     cin >> n;
     for(int i = 0; i < n; i++){
         cin >> x;
-        treap = merge(treap, get_node(x));
+        push_back(x);
     }
 
     print();
@@ -148,6 +205,17 @@ void solve(){
     cout << query(1, 3) << endl;
     erase(3, 4); print();
     insert(3, 3); print();
+
+    x = 10;
+    if(find_pos(x) == -1){
+        insert(lower_bound(x), x);
+    }
+    print();
+    
+    x = 2;
+    x = find_pos(x);
+    if(~x) erase(x, x);
+    print();
 }
 
 int32_t main()
@@ -165,3 +233,7 @@ int32_t main()
 
     return 0;
 }
+/**
+5
+1 2 3 4 5
+*/
