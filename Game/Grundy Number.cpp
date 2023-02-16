ad417e4c2ac5e08be5b67ec1b57d09b3b05badce
Kawchar Husain <52884694+kawchar85@users.noreply.github.com>
2023-02-16 12:11:55 +0600

    Update Grundy Number.cpp

--- Game/Grundy Number.cpp
+++ Game/Grundy Number.cpp
@@ -2,17 +2,61 @@
 Grundy/Nimbers: smallest number jei state a jawa jabe nah
 Problem: Divide pile in two unequal piles
 */
-int G[sz];
-void Grandy(int n){
-    set<int>s;
-    for(i=1; ; i++){
-        j=n-i;
-        if(j<=i) break;
-        //i,j te jabo. insert i,j othoba i^j
-        s.insert(G[i]^G[j]);
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ALL(v) v.begin(),v.end()
+#define clean(x,y) memset(x,y,sizeof(x));
+#define endl "\n"
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define MOD 1000000007
+#define MAX 10004
+
+typedef long long ll;
+
+int g[MAX];
+int grundy(int n) {
+    if(n < 3) return 0;
+    if(~g[n]) return g[n];
+    
+    bool vis[n+10] = {};
+    int x = 0;
+    for(int i = 1; 2*i < n; i++) {
+        x = grundy(i) ^ grundy(n - i);
+        if(x < (n+10)) vis[x] = 1;
     }
-    i=0;
-    while(s.find(i)!=s.end())
-        i++;
-    G[n]=i;
+
+    x = 0;
+    while(vis[x]) 
+        x++;
+    return g[n] = x;
+}
+void solve() {
+    int n, x, xorr = 0;
+
+    cin >> n;
+    while(n--) {
+        cin >> x;
+        xorr ^= grundy(x);
+    }
+
+    if(xorr) cout << "Alice\n";
+    else cout << "Bob\n";
+
+}
+
+int32_t main() {
+    FAST
+    clean(g, -1);
+
+    int TC = 1;
+
+    cin>>TC;
+
+    for(int cs = 1; cs <= TC; cs++) {
+        cout << "Case " << cs << ": ";
+        solve();
+    }
+
+    return 0;
 }
