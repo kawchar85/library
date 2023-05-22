310404ef120b61a32c679fa71511addfe3ce290d
Kawchar Husain <52884694+kawchar85@users.noreply.github.com>
2023-05-22 17:16:17 +0600

    fix overflow issue

--- Number Theory/Modular Arithmetic/mod_inv_till_M.cpp
+++ Number Theory/Modular Arithmetic/mod_inv_till_M.cpp
@@ -1,18 +1,18 @@
 #include<bits/stdc++.h>
-#define MAX 500
 using namespace std;
+#define MAX 500
+#define MOD 1000000007
 
 int inv[MAX];
 
-///O(m) m must be prime
-void inverse(int m)
-{
-    inv[1]=1;
-    for(int i=2; i<m; i++)
-        inv[i]=(m-(m/i)*inv[m%i]%m)%m;
+//m must be prime
+void calculateInverse(int n, int m = MOD) {
+    inv[1] = 1;
+    for(int i = 2; i <= n; i++)
+        inv[i] = (m - 1LL * (m/i) * inv[m%i]%m)%m;
 }
 
-int main()
-{
-
+int main() {
+    calculateInverse(100);
+    cout << inv[3] << endl;
 }
