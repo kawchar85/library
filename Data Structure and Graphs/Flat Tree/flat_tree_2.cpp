2ec690d47ce6dca8cc03d6ce36c1421d1aa5cce1
Kawchar Husain <52884694+kawchar85@users.noreply.github.com>
2023-10-07 02:39:31 +0600

    Update flat_tree_2.cpp

--- Data Structure and Graphs/Flat Tree/flat_tree_2.cpp
+++ Data Structure and Graphs/Flat Tree/flat_tree_2.cpp
@@ -5,32 +5,35 @@
     (L,R) in FT, is child
     of x & each node will
     appear twice.
+
+    path u to v:
+    assume in[u] <= in[v] and  p = LCA(u, v).
+    case 1: p = u, path =  [in(u), in(v)]. Notice that if x is not on the path, x occurs twice or zero times in our specified query range.
+    case 2: p != u, path = [out(u), in(v)] + [in(P), in(P)].
+    
 */
 #include<bits/stdc++.h>
 using namespace std;
 #define MAX 1001
 
 vector<int> adj[MAX];
-int FT[2*MAX],in[MAX], out[MAX];
+int FT[2*MAX], in[MAX], out[MAX];
 int Timer;
 
-void DFS(int u, int p)
-{
-    FT[Timer]=u;
+void DFS(int u, int p) {
+    FT[Timer] = u;
     in[u] = Timer++;
 
-    for(auto v: adj[u])
-    {
-        if(v==p) continue;
+    for(auto v : adj[u]) {
+        if(v == p) continue;
         DFS(v, u);
     }
 
-    FT[Timer]=u;
+    FT[Timer] = u;
     out[u] = Timer++;
 }
 
-int main()
-{
+int main() {
     int i,j,n,m;
 
     cin>>n;
