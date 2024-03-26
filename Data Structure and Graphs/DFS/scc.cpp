c2d796382fc360a8fdb6572aeffed681f4ca38eb
Kawchar Husain <52884694+kawchar85@users.noreply.github.com>
2024-03-26 16:21:43 +0600

    Update scc.cpp

--- Data Structure and Graphs/DFS/scc.cpp
+++ Data Structure and Graphs/DFS/scc.cpp
@@ -1,85 +1,92 @@
-#include<bits/stdc++.h>
-#define SZ 100
-#define pb push_back
-
-using namespace std;
-
-bool vis[SZ];
-int Time,dis[SZ],fin[SZ];
-vector<int>adj[SZ],adjR[SZ],ans;
-vector< pair<int,int> >order;
-
-void DFS(int n)
-{
-    vis[n]=1;
-    dis[n]=++Time;
-    for(auto x : adj[n])
-    {
-        if(vis[x]) continue;
-        DFS(x);
-    }
-    fin[n]=++Time;
-    order.push_back({fin[n],n});
-}
-
-void DFS2(int n)
-{
-    vis[n]=1;
-    ans.pb(n);
-    for(auto x: adjR[n])
-    {
-        if(vis[x]) continue;
-        DFS2(x);
-    }
-}
-
-
-int main()
-{
-    int n,m,x,y,i;
-    cin>>n>>m;
-    for(i=0; i<m; i++)
-    {
-        cin>>x>>y;
-        adj[x].pb(y);
-        adjR[y].pb(x);
-    }
-    memset(vis,0,sizeof vis);
-    for(i=0; i<n; i++)
-        if(!vis[i])
-            DFS(i);
-    sort(order.rbegin(), order.rend());
-    memset(vis,0,sizeof vis);
-    Time=0;
-    for(auto k : order)
-    {
-        if(!vis[k.second])
-        {
-            ans.clear();
-            DFS2(k.second);
-            cout<<"{ ";
-            for(auto K : ans)
-                cout<<K<<" ";
-            cout<<"},";
-        }
-    }
-    return 0;
-}
 /**
-13 14
-0 1
-0 4
-8 10
-1 3
-7 9
-4 3
-8 7
-11 2
-10 12
-0 5
-3 6
-5 6
-7 12
-3 5
+    "If you have an apple and I have an apple and
+    we exchange apples then you and I will still
+    each have one apple. But if you have an idea
+    and I have an idea and we exchange these
+    ideas, then each of us will have two ideas."
+                        -George Bernard Shaw
 */
 
+//https://codeforces.com/contest/427/problem/C
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define MOD 1000000007
+#define MAX 300005
+
+typedef long long ll;
+
+vector<int> adj[MAX], adjR[MAX];
+int cost[MAX];
+bool vis[MAX];
+vector<int> cur_scc, order;
+
+void dfs(int u) {
+    vis[u] = 1;
+    for(int v : adj[u]) {
+        if(!vis[v]) dfs(v);
+    }
+    order.push_back(u);
+}
+
+void find_scc(int u) {
+    cur_scc.push_back(u);
+    vis[u] = 1;
+    for(int v : adjR[u]) {
+        if(!vis[v]) find_scc(v);
+    }
+}
+
+void solve() {
+    int n; cin >> n;
+    for(int i = 1; i <= n; i++) {
+        cin >> cost[i];
+    }
+    int m; cin >> m;
+    while(m--) {
+        int x, y; cin >> x >> y;
+        adj[x].push_back(y);
+        adjR[y].push_back(x);
+    }
+    memset(vis, 0, sizeof vis);
+    for(int i = 1; i <= n; i++) {
+        if(!vis[i]) dfs(i);
+    }
+    ll total = 0, way = 1;
+    memset(vis, 0, sizeof vis);
+    while(order.size()) {
+        int u = order.back();
+        order.pop_back();
+        if(vis[u]) continue;
+        cur_scc.clear();
+        find_scc(u);
+
+        int mn = INT_MAX, cnt = 0;
+        for(auto u : cur_scc) {
+            if(cost[u] < mn) {
+                cnt = 0;
+                mn = cost[u];
+            }
+            if(cost[u] == mn)
+                cnt++;
+        }
+        total += mn;
+         way = (way * cnt) % MOD;
+    }
+    cout << total << " " << way << "\n";
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    int TC = 1;
+
+    // cin >> TC;
+
+    for(int cs = 1; cs <= TC; cs++) {
+        //cout << "Case " << cs << ": ";
+        solve();
+    }
+
+    return 0;
+}
