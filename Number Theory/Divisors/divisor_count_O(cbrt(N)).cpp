98b6ba90351d7d8fa3b24d2477f99a754abb11c5
Kawchar Husain <kawchar85@yahoo.com>
2025-07-05 03:39:00 +0600

    fix error & --> %

--- Number Theory/Divisors/divisor_count_O(cbrt(N)).cpp
+++ Number Theory/Divisors/divisor_count_O(cbrt(N)).cpp
@@ -1,60 +1,78 @@
-#include<bits/stdc++.h>
-#define ll long long
-#define MAX 1004
+#include <bits/stdc++.h>
 using namespace std;
 
-bool siv[MAX];
-bool isPrimeSq[MAX*MAX+1],isPrime[MAX];
-vector<int> prime;
+const int MAX_PRIME_LIMIT = 1000000;
 
-///O(n*lg(lg(n)))
-void sieve()
-{
-    ll i,j;
-    for(i=3; i*i<MAX; i+=2)
-        if(!siv[i])
-            for(j=i*i; j<MAX; j+=i+i)
-                siv[j]=1;
-    prime.push_back(2);
-    isPrime[2]=1;
-    isPrimeSq[2*2]=1;
-    for(i=3; i<MAX; i+=2){
-        if(!siv[i]){
-            prime.push_back(i);
-            isPrime[i]=1;
-            isPrimeSq[i*i]=1;
+vector<int> primes;
+vector<bool> isPrime;
+vector<bool> isPerfectSquareOfPrime;
+
+long long cube(int x) {
+    return (long long)x * x * x;
+}
+
+void generatePrimesUsingSieve() {
+    vector<bool> isComposite(MAX_PRIME_LIMIT, false);
+    isPrime.assign(MAX_PRIME_LIMIT, false);
+    isPerfectSquareOfPrime.assign((long long)MAX_PRIME_LIMIT * MAX_PRIME_LIMIT + 1, false);
+    
+    for (int i = 3; i * i < MAX_PRIME_LIMIT; i += 2) {
+        if (!isComposite[i]) {
+            for (int j = i * i; j < MAX_PRIME_LIMIT; j += 2 * i) {
+                isComposite[j] = true;
+            }
+        }
+    }
+    
+    primes.push_back(2);
+    isPrime[2] = true;
+    isPerfectSquareOfPrime[4] = true;
+    
+    for (int i = 3; i < MAX_PRIME_LIMIT; i += 2) {
+        if (!isComposite[i]) {
+            primes.push_back(i);
+            isPrime[i] = true;
+            long long primeSquare = (long long)i * i;
+            if (primeSquare < isPerfectSquareOfPrime.size()) {
+                isPerfectSquareOfPrime[primeSquare] = true;
+            }
         }
     }
 }
 
-int CB(int x)
-{
-    return x*x*x;
-}
-
-//O(N^1/3)
-int NOD(int n)
-{
-    int cnt=1;
-    for(int i=0; CB(prime[i])<=n; i++)
-    {
-        int c=1;
-        while(n&prime[i]==0)
-        {
-            c++;
-            n/=prime[i];
+int countDivisors(long long n) {
+    if (n <= 0) return 0;
+    if (n == 1) return 1;
+    
+    int divisorCount = 1;
+    long long remainingNumber = n;
+    
+    for (int i = 0; i < primes.size() && cube(primes[i]) <= remainingNumber; i++) {
+        int currentPrime = primes[i];
+        int exponent = 0;
+        
+        while (remainingNumber % currentPrime == 0) {
+            exponent++;
+            remainingNumber /= currentPrime;
         }
-        cnt*=c;
+        
+        if (exponent > 0) {
+            divisorCount *= (exponent + 1);
+        }
     }
-
-    if(isPrime[n]) cnt*=2;
-    else if(isPrimeSq[n]) cnt*=3;
-    else if(n!=1) cnt*=4;
-    return cnt;
-}
-int main()
-{
-    sieve(); //need prime till n^1/3
-    cout<<NOD(10);
-    return 0;
+    
+    if (remainingNumber == 1) {
+        
+    }
+    else if (remainingNumber < isPrime.size() && isPrime[remainingNumber]) {
+        divisorCount *= 2;
+    }
+    else if (remainingNumber < isPerfectSquareOfPrime.size() && isPerfectSquareOfPrime[remainingNumber]) {
+        divisorCount *= 3;
+    }
+    else if (remainingNumber != 1) {
+        divisorCount *= 4;
+    }
+    
+    return divisorCount;
 }
